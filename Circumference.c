82d27ca41461ec515c59a520523357e56ea8121e
navprit26K <navpritkarade4939@gmail.com>
2026-05-28 11:19:47 +0530

    Add files via upload

---
+++ Circumference.c
@@ -0,0 +1,16 @@
+// Name- Navprit Karade
+// PRN no-2503033111372039
+
+
+#include<stdio.h>
+int main(){
+      float D,r;
+      printf("Enter daimeter: ");
+      scanf("%f",&D);
+      r=D/2;
+//Area of circle
+      printf("Area of circle is %f\n",3.14*r*r);
+//circumferance of circle
+      printf("circumferance of circle is %f: ",2*3.14*r);
+      return 0;
+}
