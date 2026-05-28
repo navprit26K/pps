55f2ab82ef42d844c0590f06342d364ffdcdd97a
navprit26K <navpritkarade4939@gmail.com>
2026-05-28 11:22:19 +0530

    Add files via upload

---
+++ Star1.c
@@ -0,0 +1,17 @@
+  // Name- Navprit Karade
+// PRN no-2503033111372039
+
+#include <stdio.h>
+int main(){
+    int i,j,k;
+    for(i=0;i<5;i++){
+        for(k=0 ;k<5-i;k++){
+
+        }
+
+        for(j=0 ;j<5-i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
