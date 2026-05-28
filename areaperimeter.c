c8fa38bab517abc145e69bf9b7c7915cfd66d2b3
navprit26K <navpritkarade4939@gmail.com>
2026-05-28 11:18:50 +0530

    Add files via upload

---
+++ areaperimeter.c
@@ -0,0 +1,17 @@
+// Name- Navprit Karade
+// PRN no-2503033111372039
+
+#include<stdio.h>
+int main(){
+      int a,b;
+      printf("Enter Length: ");
+      scanf("%d",&a);
+
+       printf("Enter Breadth: ");
+      scanf("%d",&b);
+//Area of Rectanngle
+      printf("Area of rectangle: %d\n",a*b);
+//perimerter OF rectangle
+      printf("Perimeter of Rectangle: %d", 2*(a+b));
+      return 0;
+}
