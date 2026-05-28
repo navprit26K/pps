b28c3f1e98a122a16d476cb73ba77a8215a562aa
navprit26K <navpritkarade4939@gmail.com>
2026-05-28 11:20:23 +0530

    Add files via upload

---
+++ Fabonacci.c
@@ -0,0 +1,21 @@
+// Name- Navprit Karade
+// PRN no-2503033111372039
+
+
+#include <stdio.h>
+
+int main() {
+    int n, a = 1, b = 1, next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
